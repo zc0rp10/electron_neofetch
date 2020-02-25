77d112bf3fc03516b9462ae5449a87b838bb3c25
zc0rp10 <zcorpio@gmail.com>
2020-02-25 15:58:32 +0100

    added systeminformationJS library

--- src/preload.js
+++ src/preload.js
@@ -12,12 +12,13 @@
 // })
 
 const os = require("os");
-const si = require("systeminformation")
+const si = require("systeminformation");
 const { screen } = require("electron").remote;
 const { spawn } = require("child_process");
 
 let pkgCount = [];
 
+//Get the number of installed packages on the system from terminal command line
 const getPackages = () => {
   const cliCommand = spawn("pacman", ["-Qq"]);
   cliCommand.stdout.on("data", data => {
@@ -27,11 +28,11 @@ const getPackages = () => {
 };
 getPackages();
 
-function secondsToHms() {
-  d = os.uptime();
-  var h = Math.floor(d / 3600);
-  var m = Math.floor((d % 3600) / 60);
-
+//Get uptime is seconds from nodeJS then convert to hours and minutes
+function secondsToHm() {
+  t = os.uptime();
+  var h = Math.floor(t / 3600);
+  var m = Math.floor((t % 3600) / 60);
   var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
   var mDisplay = m > 0 ? m + (m == 1 ? " minute" : " minutes") : "";
   return hDisplay + mDisplay;
@@ -41,14 +42,22 @@ window.addEventListener("DOMContentLoaded", () => {
   $ = document.getElementById.bind(document);
   $("username").innerHTML = os.userInfo(username).username;
   $("hostname").innerHTML = os.hostname();
-  $("os").innerHTML = os.release();
-  //$("host").innerHTML = os.release();
+
   $("kernel").innerHTML = os.release();
-  $("uptime").innerHTML = secondsToHms();
+  $("uptime").innerHTML = secondsToHm();
   $("packages").innerHTML = pkgCount.length;
   $("shell").innerHTML = os.userInfo(username).shell;
   $("resolution").innerHTML = `${screen.getPrimaryDisplay().size.width} x ${
     screen.getPrimaryDisplay().size.height
   }`;
   $("memory").innerHTML = Math.round(os.totalmem() / 1000000) + " " + "MB";
+
+  si.system().then(data => {
+    $("host").innerHTML = `${data.model} ${data.version}`;
+  });
+
+  si.osInfo().then(data => {
+    console.log(data);
+    $("os").innerHTML = `${data.distro} ${data.arch}`;
+  });
 });
